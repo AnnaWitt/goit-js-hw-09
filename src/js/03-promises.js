58aaa7c9de9f09a364a4912e6c9e084471e476dd
Anna Witt <110746108+AnnaWitt@users.noreply.github.com>
2023-05-29 00:32:06 +0200

    Update 03-promises.js

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,41 @@
+const form = document.querySelector('.form');
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
   if (shouldResolve) {
-    // Fulfill
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        resolve({ position, delay });
+      }, delay);
+    });
   } else {
-    // Reject
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        reject({ position, delay });
+      }, delay);
+    });
   }
 }
+
+form.addEventListener('submit', event => {
+  event.preventDefault();
+
+  const delay = parseInt(event.target.elements.delay.value);
+  const step = parseInt(event.target.elements.step.value);
+  const amount = parseInt(event.target.elements.amount.value);
+
+  let currentPosition = 1;
+  let currentDelay = 1;
+
+  for (let i = 1; i <= amount; i++) {
+    createPromise(currentPosition, delay)
+      .then(({ position, delay }) => {
+        console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      })
+      .catch(({ position, delay }) => {
+        console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+      });
+    currentPosition++;
+    currentDelay += step;
+  }
+});
